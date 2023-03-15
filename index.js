76e14acddec4063f8bc7c56a4e0f81838ba786d9
Georgina <faithwangare750@gmail.com>
2023-03-15 15:17:53 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Gina Wangari';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+let shortGreeting = 'Welcome, ';
+const firstInitial= currentUser[0];
+const restOfName=currentUser.slice(1);
+shortGreeting = shortGreeting + firstInitial + "!";
